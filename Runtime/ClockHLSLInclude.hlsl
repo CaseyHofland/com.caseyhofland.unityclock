a055d8f750c5436663c55307f928ecc0a32b15f5
CaseyDeCoder <hofland.casey@gmail.com>
2023-04-18 11:07:28 +0200

    The HLSL file took float4, should be int4

--- Runtime/ClockHLSLInclude.hlsl
+++ Runtime/ClockHLSLInclude.hlsl
@@ -1,18 +1,18 @@
 //UNITY_SHADER_NO_UPGRADE
 #ifndef CLOCKHLSLINCLUDE_INCLUDED
 #define CLOCKHLSLINCLUDE_INCLUDED
-float4 _UnityClock_Clock_Time;
+int4 _UnityClock_Clock_Time;
 float _UnityClock_Clock_Interpolant;
 float _UnityClock_Clock_PingPong;
 
-void ClockValues_float(out float4 time, out float interpolant, out float pingPong)
+void ClockValues_float(out int4 time, out float interpolant, out float pingPong)
 {
 	time = _UnityClock_Clock_Time;
 	interpolant = _UnityClock_Clock_Interpolant;
 	pingPong = _UnityClock_Clock_PingPong;
 }
 
-void ClockValues_half(out float4 time, out half interpolant, out half pingPong)
+void ClockValues_half(out int4 time, out half interpolant, out half pingPong)
 {
 	time = _UnityClock_Clock_Time;
 	interpolant = _UnityClock_Clock_Interpolant;
