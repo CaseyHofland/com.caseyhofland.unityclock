6f1382637a7854349a39cbd3f01634d27649d0c7
CaseyHofland <hofland.casey@gmail.com>
2023-08-11 23:17:08 +0200

    Add UIElements implementation
    TimeOnlyField and TimeSpanField are proper implementations for TimeOnly and TimeSpan in the inspector. The field can be dragged in order to increase or decrease the time field. Naturally, inputting a value via text still works as well.
    
    Additionally, the TimeOnlyAttribute and TimeSpanAttribute have been simplified. They can no longer take any additional values and instead always display the foldout. In most cases, the UIElement implementation will be used, so this is not a real issue.

--- Editor/TimeOnlyDrawer.cs
+++ Editor/TimeOnlyDrawer.cs
@@ -1,46 +1,87 @@
 #nullable enable
 using System;
+using System.Reflection;
+using UnityClock.UIElements;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace UnityClock.Editor
 {
     [CustomPropertyDrawer(typeof(TimeOnlyAttribute), true)]
     public class TimeOnlyDrawer : PropertyDrawer
     {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            // Check if property is of type long.
+            if (property.numericType != SerializedPropertyNumericType.Int64)
+            {
+                return base.CreatePropertyGUI(property);
+            }
+
+            // Create the TimeOnlyField.
+            var timeOnlyField = new TimeOnlyField(preferredLabel)
+            {
+                isDelayed = fieldInfo.GetCustomAttribute<DelayedAttribute>(true) != null,
+            };
+
+            // Bind the property.
+            timeOnlyField.RegisterCallback<BlurEvent>(OnBlur);
+            timeOnlyField.TrackPropertyValue(property, PropertyChanged);
+            PropertyChanged(property);
+
+            // Return the TimeSpanField.
+            timeOnlyField.AddToClassList(TimeOnlyField.alignedFieldUssClassName);
+            return timeOnlyField;
+
+            // Bind property methods.
+            void OnBlur(BlurEvent blurEvent)
+            {
+                property.longValue = timeOnlyField.value.Ticks;
+                property.serializedObject.ApplyModifiedProperties();
+            }
+
+            void PropertyChanged(SerializedProperty property)
+            {
+                timeOnlyField.SetValueWithoutNotify(new TimeOnly(property.longValue));
+            }
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            // Check if property is of type long.
             if (property.numericType != SerializedPropertyNumericType.Int64)
             {
                 base.OnGUI(position, property, label);
+                return;
             }
 
             using var _ = new EditorGUI.PropertyScope(position, label, property);
             var timeOnly = new TimeOnly(property.longValue);
-            var timeOnlyAttribute = (TimeOnlyAttribute)attribute;
-
             position.height = EditorGUIUtility.singleLineHeight;
+
             // Draw foldout and text field.
             {
                 var foldoutLabel = new GUIContent(label);
                 var textFieldPosition = new Rect(position);
                 textFieldPosition.x += EditorGUIUtility.labelWidth - EditorGUI.indentLevel * 18f;
                 textFieldPosition.width = position.width - EditorGUIUtility.labelWidth + EditorGUI.indentLevel * 18f;
+
                 EditorGUI.BeginChangeCheck();
-                var timeString = EditorGUI.DelayedTextField(textFieldPosition, timeOnly.ToString(timeOnlyAttribute.timeFormat));
+                var timeString = (fieldInfo.GetCustomAttribute<DelayedAttribute>(true) != null)
+                    ? EditorGUI.DelayedTextField(position, label, timeOnly.ToString("g"))
+                    : EditorGUI.TextField(position, label, timeOnly.ToString("g"));
                 if (!timeString.Contains(":"))
                 {
-                    timeString += ":0";
+                    timeString += ":00";
                 }
-                if (EditorGUI.EndChangeCheck() && TimeSpan.TryParse(timeString, out var result))
+                if (EditorGUI.EndChangeCheck() && TimeSpan.TryParse("0." + timeString, out var result))
                 {
                     timeOnly = TimeOnly.MinValue.Add(result);
                 }
 
-                if (timeOnlyAttribute.showHour || timeOnlyAttribute.showMinute || timeOnlyAttribute.showSecond || timeOnlyAttribute.showMillisecond || timeOnlyAttribute.showInterpolant)
-                {
-                    property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, foldoutLabel, true);
-                }
+                property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, foldoutLabel, true);
             }
 
             // Draw sliders.
@@ -53,22 +94,18 @@ namespace UnityClock.Editor
                     EditorGUI.BeginChangeCheck();
 
                     timeOnly.Deconstruct(out int hour, out int minute, out int second, out int millisecond);
-                    if (timeOnlyAttribute.showHour)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         hour = EditorGUI.IntSlider(position, nameof(timeOnly.Hour), hour, 0, 23);
                     }
-                    if (timeOnlyAttribute.showMinute)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         minute = EditorGUI.IntSlider(position, nameof(timeOnly.Minute), minute, 0, 59);
                     }
-                    if (timeOnlyAttribute.showSecond)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         second = EditorGUI.IntSlider(position, nameof(timeOnly.Second), second, 0, 59);
                     }
-                    if (timeOnlyAttribute.showMillisecond)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         millisecond = EditorGUI.IntSlider(position, nameof(timeOnly.Millisecond), millisecond, 0, 999);
@@ -80,7 +117,6 @@ namespace UnityClock.Editor
                 }
 
                 // Draw interpolant
-                if (timeOnlyAttribute.showInterpolant) 
                 {
                     EditorGUI.BeginChangeCheck();
                     position.y += EditorGUIUtility.singleLineHeight + 2f;
@@ -100,33 +136,11 @@ namespace UnityClock.Editor
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            var timeOnlyAttribute = (TimeOnlyAttribute)attribute;
-
             var height = EditorGUI.GetPropertyHeight(property, label, true);
             if (property.isExpanded)
             {
-                if (timeOnlyAttribute.showHour)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeOnlyAttribute.showMinute)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeOnlyAttribute.showSecond)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeOnlyAttribute.showMillisecond)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeOnlyAttribute.showInterpolant)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
+                height += (EditorGUIUtility.singleLineHeight + 2f) * 5f;
             }
-
             return height;
         }
     }
--- Editor/TimeSpanDrawer.cs
+++ Editor/TimeSpanDrawer.cs
@@ -1,46 +1,83 @@
 #nullable enable
 using System;
+using System.Reflection;
+using UnityClock.UIElements;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace UnityClock.Editor
 {
     [CustomPropertyDrawer(typeof(TimeSpanAttribute), true)]
     public class TimeSpanDrawer : PropertyDrawer
     {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            // Check if property is of type long.
+            if (property.numericType != SerializedPropertyNumericType.Int64)
+            {
+                return base.CreatePropertyGUI(property);
+            }
+
+            // Create the TimeSpanField.
+            var timeSpanField = new TimeSpanField(preferredLabel)
+            {
+                isDelayed = fieldInfo.GetCustomAttribute<DelayedAttribute>(true) != null,
+            };
+
+            // Bind the property.
+            timeSpanField.RegisterCallback<BlurEvent>(OnBlur);
+            timeSpanField.TrackPropertyValue(property, PropertyChanged);
+            PropertyChanged(property);
+
+            // Return the TimeSpanField.
+            timeSpanField.AddToClassList(TimeSpanField.alignedFieldUssClassName);
+            return timeSpanField;
+
+            // Bind property methods.
+            void OnBlur(BlurEvent blurEvent)
+            {
+                property.longValue = timeSpanField.value.Ticks;
+                property.serializedObject.ApplyModifiedProperties();
+            }
+
+            void PropertyChanged(SerializedProperty property)
+            {
+                timeSpanField.SetValueWithoutNotify(new TimeSpan(property.longValue));
+            }
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            // Check if property is of type long.
             if (property.numericType != SerializedPropertyNumericType.Int64)
             {
                 base.OnGUI(position, property, label);
+                return;
             }
 
             using var _ = new EditorGUI.PropertyScope(position, label, property);
             var timeSpan = new TimeSpan(property.longValue);
-            var timeSpanAttribute = (TimeSpanAttribute)attribute;
-
             position.height = EditorGUIUtility.singleLineHeight;
+
             // Draw foldout and text field.
             {
                 var foldoutLabel = new GUIContent(label);
                 var textFieldPosition = new Rect(position);
                 textFieldPosition.x += EditorGUIUtility.labelWidth - EditorGUI.indentLevel * 18f;
                 textFieldPosition.width = position.width - EditorGUIUtility.labelWidth + EditorGUI.indentLevel * 18f;
+
                 EditorGUI.BeginChangeCheck();
-                var timeString = EditorGUI.DelayedTextField(textFieldPosition, timeSpan.ToString(timeSpanAttribute.timeFormat));
-                if (!timeString.Contains(":"))
-                {
-                    timeString += ":0";
-                }
+                var timeString = (fieldInfo.GetCustomAttribute<DelayedAttribute>(true) != null)
+                    ? EditorGUI.DelayedTextField(position, label, timeSpan.ToString("g"))
+                    : EditorGUI.TextField(position, label, timeSpan.ToString("g"));
                 if (EditorGUI.EndChangeCheck() && TimeSpan.TryParse(timeString, out var result))
                 {
                     timeSpan = result;
                 }
 
-                if (timeSpanAttribute.showDays || timeSpanAttribute.showHours || timeSpanAttribute.showMinutes || timeSpanAttribute.showSeconds || timeSpanAttribute.showMilliseconds)
-                {
-                    property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, foldoutLabel, true);
-                }
+                property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, foldoutLabel, true);
             }
 
             // Draw sliders.
@@ -54,32 +91,26 @@ namespace UnityClock.Editor
 
                     bool minus = timeSpan.Ticks < 0;
                     int days = Math.Abs(timeSpan.Days), hours = Math.Abs(timeSpan.Hours), minutes = Math.Abs(timeSpan.Minutes), seconds = Math.Abs(timeSpan.Seconds), milliseconds = Math.Abs(timeSpan.Milliseconds);
-                    if (timeSpanAttribute.showMinus)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
-                        minus = EditorGUI.Toggle(position, ObjectNames.NicifyVariableName("minusThing"), minus);
+                        minus = EditorGUI.Toggle(position, ObjectNames.NicifyVariableName(nameof(minus)), minus);
                     }
-                    if (timeSpanAttribute.showDays)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         days = EditorGUI.IntField(position, nameof(TimeSpan.Days), days);
                     }
-                    if (timeSpanAttribute.showHours)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         hours = EditorGUI.IntSlider(position, nameof(timeSpan.Hours), hours, 0, 23);
                     }
-                    if (timeSpanAttribute.showMinutes)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         minutes = EditorGUI.IntSlider(position, nameof(timeSpan.Minutes), minutes, 0, 59);
                     }
-                    if (timeSpanAttribute.showSeconds)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         seconds = EditorGUI.IntSlider(position, nameof(timeSpan.Seconds), seconds, 0, 59);
                     }
-                    if (timeSpanAttribute.showMilliseconds)
                     {
                         position.y += EditorGUIUtility.singleLineHeight + 2f;
                         milliseconds = EditorGUI.IntSlider(position, nameof(timeSpan.Milliseconds), milliseconds, 0, 999);
@@ -102,37 +133,11 @@ namespace UnityClock.Editor
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            var timeSpanAttribute = (TimeSpanAttribute)attribute;
-
             var height = EditorGUI.GetPropertyHeight(property, label, true);
             if (property.isExpanded)
             {
-                if (timeSpanAttribute.showMinus)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeSpanAttribute.showDays)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeSpanAttribute.showHours)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeSpanAttribute.showMinutes)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeSpanAttribute.showSeconds)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
-                if (timeSpanAttribute.showMilliseconds)
-                {
-                    height += EditorGUIUtility.singleLineHeight + 2f;
-                }
+                height += (EditorGUIUtility.singleLineHeight + 2f) * 6f;
             }
-
             return height;
         }
     }
